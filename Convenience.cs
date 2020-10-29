4ae71d2a76bbbb026b7490079eb2e20885c70a87
kgibbsdev <54226327+kgibbsdev@users.noreply.github.com>
2020-10-29 02:04:49 -0400

    Separated convenience functions into their own file. Added CSV writing of Records class. Added option checking, added PrintInstructions convenience function.

---
+++ Convenience.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Xml;
+
+namespace DotNetXMLConverter
+{
+    class Convenience
+    {
+        //Convenience function that creates a line break.
+        public static void LineBreak(int breaks = 1)
+        {
+            for (int i = 0; i < breaks; i++)
+            {
+                Console.WriteLine('\n');
+            }
+        }
+
+        //Convenience function that logs a string to the console.
+        public static void Print(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public static string GetFolderPath()
+        {
+            //Gets the path of the .exe file
+            string path = System.IO.Directory.GetCurrentDirectory();
+
+            //Get the parent folder that contains the project.
+            path = Path.Combine(path, "..", "..", "..", @"..\");
+            Print("Set path to: " + path);
+            return path;
+        }
+
+        public static void PrintInstructions()
+        {
+            Print("Press 1 to generate a CSV file for translation.");
+            Print("Press 2 to read an already-translated CSV file.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,54 +1,69 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Xml;
+
+//This is where the convenience functions LineBreak(), Print(), GetFolderPath(), and PrintInstructions() come from.
+using static DotNetXMLConverter.Convenience;
 
 namespace DotNetXMLConverter
 {
     public class Record
     {
         public string UID { get; set; }
+        public string FileName { get; set; }
         public string OriginalText { get; set; }
-        //TODO: Decide if there should be two separate classes for translated/non-translated records
-        public string TranslatedText { get; set; }
+      
 
-        public Record(string uid, string originalText, string translatedText = "") 
+        public Record(string _uid,  string _fileName, string _originalText)
         {
-            UID = uid;
-            OriginalText = originalText;
-            TranslatedText = translatedText; 
+            UID = _uid;
+            FileName = _fileName;
+            OriginalText = _originalText;
         }
     }
 
     class ReadFromFile
     {
-        //Convenience function that creates a line break.
-        public static void LineBreak(int breaks = 1)
+        public static string csvFilePath;
+        public static string csvDirectoryPath;
+
+        static List<Record> recordList = new List<Record>();
+        public static void CreateCSV(string path)
         {
-            for (int i = 0; i <= breaks; i++)
+            Print("path: " + path);
+            Print("Checking " + path + @" for csvtest.csv.");
+
+            csvDirectoryPath = path + @"CSVs";
+            csvFilePath = path + @"\CSVs\csvtest.csv";
+
+            if (!Directory.Exists(csvDirectoryPath))
             {
-                Console.WriteLine('\n');
+                Print("Creating CSV directory.");
+                Directory.CreateDirectory(csvDirectoryPath);
+            }
+            else
+            {
+                Print("CSV Directory already found");
+            }
+
+            if (!File.Exists(path + @"\CSVs\csvtest.csv"))
+            {
+                Print("Creating CSV File.");
+                var csvFile = File.Create(csvFilePath);
+                csvFile.Close();
+            }
+            else
+            {
+                Print("CSV File found");
             }
         }
 
-        //Convenience function that logs a string to the console.
-        public static void Print(string text)
-        {
-            Console.WriteLine(text);
-        }
-
-        public static string GetFolderPath()
-        {
-            //Gets the path of the .exe file
-            string path = System.IO.Directory.GetCurrentDirectory();
-
-            //Get the parent folder that contains the project.
-            path = Path.Combine(path, "..", "..", "..", "..");
-            Print("Set path to: " + path);
-            return path;
-        }
-
-        public static void GetTextBreakContent(XmlDocument document)
+        //Gets all of the content between Text_Break tags
+        public static void GetTextBreakContent(XmlDocument document, string fileName)
         {
             XmlNodeList textBreakNodeList;
             XmlNode root = document.DocumentElement;
@@ -56,21 +71,26 @@ namespace DotNetXMLConverter
 
             for (int j = 0; j < textBreakNodeList.Count; j++)
             {
-                Print(textBreakNodeList[j].FirstChild.InnerText);
                 //Skip to the last node within the Text_Break tag
+                XmlNode firstChild = textBreakNodeList[j].FirstChild;
                 XmlNode lastChild = textBreakNodeList[j].LastChild;
-                Print(lastChild.InnerText);
+                string textBreakContent = firstChild.InnerText;
 
                 //The last node within a Text_Break will always be the soundeffect tag, and the previous sibling of the sound effect
                 //node is the UID tag for the text break.
-                string TextBreakUID = lastChild.PreviousSibling.InnerText;
-
-                Print(TextBreakUID);
-                LineBreak();
+                string textBreakUID = lastChild.PreviousSibling.InnerText;
+                Print(fileName);
+                //If the text_break does not have any content, skip adding it to the list of records.
+                if (textBreakContent != "")
+                {
+                    Record newRecord = new Record(textBreakUID, fileName, textBreakContent);
+                    recordList.Add(newRecord);
+                }
             }
         }
 
-        public static void GetEventOptionContent(XmlDocument document)
+        //Gets all of the content within Event_Option tags
+        public static void GetEventOptionContent(XmlDocument document, string fileName)
         {
             XmlNodeList eventOptionNodeList;
             XmlNode root = document.DocumentElement;
@@ -78,42 +98,75 @@ namespace DotNetXMLConverter
 
             for (int i = 0; i < eventOptionNodeList.Count; i++)
             {
-                string optionText = eventOptionNodeList[i].FirstChild.InnerText;
-                string eventOptionUID = eventOptionNodeList[i].LastChild.InnerText;
+                string optionContent = eventOptionNodeList[i].FirstChild.InnerText;
+
+                string eventOptionUID;
+                //There is one case where the Last Child of the event_option tag is NOT a UID
+                //Therefore, If the Lastchild is empty, check the previous sibling.
+                if (eventOptionNodeList[i].LastChild.InnerText != "")
+                {
+                    eventOptionUID = eventOptionNodeList[i].LastChild.InnerText;
+                }
+                else
+                {
+                    eventOptionUID = eventOptionNodeList[i].LastChild.PreviousSibling.InnerText;
+                }
+
+                //If the option_text does not have any content, skip adding it to the list of records.
+                if (optionContent != "")
+                {
+                    Record newRecord = new Record(eventOptionUID, fileName, optionContent );
+                    recordList.Add(newRecord);
+                }
+            }
+        }
+
+        public static void WriteRecordsToCSV()
+        {
+            using (var writer = new StreamWriter(csvFilePath))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(recordList);
             }
         }
 
         static void Main()
         {
-           string path = GetFolderPath();
-           string[] files = System.IO.Directory.GetFiles(path);
+            PrintInstructions();
 
-            //Console.WriteLine(path);
+            ConsoleKey option = Console.ReadKey().Key;
 
-            foreach(string filePath in files)
+            while (option != ConsoleKey.D1 && option != ConsoleKey.D2)
+            {
+                LineBreak();
+                PrintInstructions();
+                LineBreak();
+                option = Console.ReadKey().Key;
+            }
+
+            string path = GetFolderPath();
+            string[] files = System.IO.Directory.GetFiles(path);
+            CreateCSV(GetFolderPath());
+
+            foreach (string filePath in files)
             {
                 if (filePath.EndsWith(".xml"))
                 {
                     XmlDocument document = new XmlDocument();
                     string text = File.ReadAllText(filePath);
-
-                    Print(filePath);
-
-                    Print("BEGIN READ OF  " + filePath);
-                    LineBreak();
+                    string fileName = Path.GetFileName(filePath);
                     document.Load(filePath);
 
-                    GetTextBreakContent(document);
-                    GetEventOptionContent(document);
-
-                    Print("END READ OF  " + filePath);
-                    LineBreak();
+                    GetTextBreakContent(document, fileName);
+                    GetEventOptionContent(document, fileName);
+                    WriteRecordsToCSV();
                 }
             }
 
+            Print("Done.");
+
             // Keep the console window open in debug mode.
-            Console.WriteLine("Done.");
-            System.Console.ReadKey();
+            Console.ReadKey();
         }
     }
 }
